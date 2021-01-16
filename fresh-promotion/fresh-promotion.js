4ecd22f2e2e8cb566b5d017cb3ad2e7ad5956fe9
Zhaoxian Hu <huzhx@uci.edu>
2021-01-16 00:40:12 -0800

    add fresh-promotion

---
+++ fresh-promotion/fresh-promotion.js
@@ -0,0 +1,26 @@
+const isWinner = (codeList, shoppingCart) => {
+  let codeListIndex = 0;
+  for (let i = 0; i < shoppingCart.length; i++) {
+    if (shoppingCart[i] === codeList[codeListIndex][0]) {
+      if (canMatch(i, codeList[codeListIndex])) {
+        i += codeList[codeListIndex].length - 1;
+        codeListIndex++;
+      }
+    }
+  }
+
+  return codeListIndex === codeList.length ? 1 : 0;
+
+  function canMatch(shoppingCartIndex, codeListSubarray) {
+    for (let i = 0; i < codeListSubarray.length; i++) {
+      if (codeListSubarray[i] === 'anything' || codeListSubarray[i] === shoppingCart[shoppingCartIndex]) {
+        shoppingCartIndex++;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+export default isWinner;
---
+++ fresh-promotion/fresh-promotion.test.js
@@ -0,0 +1,45 @@
+import isWinner from './fresh-promotion';
+
+test('test1', () => {
+  const codeList = [
+    ['apple', 'apple'],
+    ['banana', 'anything', 'banana'],
+  ];
+  const shoppingCart = ['orange', 'apple', 'apple', 'banana', 'orange', 'banana'];
+  const result = isWinner(codeList, shoppingCart);
+  const expected = 1;
+  expect(result).toBe(expected);
+});
+
+test('test2', () => {
+  const codeList = [
+    ['apple', 'apple'],
+    ['banana', 'anything', 'banana'],
+  ];
+  const shoppingCart = ['banana', 'orange', 'banana', 'apple', 'apple'];
+  const result = isWinner(codeList, shoppingCart);
+  const expected = 0;
+  expect(result).toBe(expected);
+});
+
+test('test3', () => {
+  const codeList = [
+    ['apple', 'apple'],
+    ['banana', 'anything', 'banana'],
+  ];
+  const shoppingCart = ['apple', 'banana', 'apple', 'banana', 'orange', 'banana'];
+  const result = isWinner(codeList, shoppingCart);
+  const expected = 0;
+  expect(result).toBe(expected);
+});
+
+test('test4', () => {
+  const codeList = [
+    ['apple', 'apple'],
+    ['apple', 'apple', 'banana'],
+  ];
+  const shoppingCart = ['apple', 'apple', 'apple', 'banana'];
+  const result = isWinner(codeList, shoppingCart);
+  const expected = 0;
+  expect(result).toBe(expected);
+});
