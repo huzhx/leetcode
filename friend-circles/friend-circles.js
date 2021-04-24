b89f3269d034aa9523380a5cf960d14f078aa071
Zhaoxian Hu <huzhx@uci.edu>
2021-04-24 14:07:18 -0700

    use the optimized approach

--- friend-circles/friend-circles.js
+++ friend-circles/friend-circles.js
@@ -1,7 +1,7 @@
-import { UnionFindByQuickUnion } from '../union-find/union-find';
+import { UnionFindByQuickUnionWithRankOpt } from '../union-find/union-find';
 
 const friendCircles = (relationships) => {
-  const unionFind = new UnionFindByQuickUnion(relationships.length);
+  const unionFind = new UnionFindByQuickUnionWithRankOpt(relationships.length);
   for (let i = 0; i < relationships.length; i++) {
     for (let j = i + 1; j < relationships[i].length; j++) {
       if (relationships[i][j] === 1) {
